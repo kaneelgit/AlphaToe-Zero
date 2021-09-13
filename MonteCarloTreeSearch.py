9610c7ff84083f50261e2af5c31062ca7a800744
Kaneel <kaneel.senevirathne@gmail.com>
2021-09-13 07:03:07 -0400

    started

--- MonteCarloTreeSearch.py
+++ MonteCarloTreeSearch.py
@@ -3,17 +3,23 @@
 
 # In[ ]:
 
-
-
 class Node:
     
-    def __init__(self, move, parent):
+    def __init__(self, move = None, parent = None):
         
         self.move = move
         self.parent = parent
-        self.children = []
+        self.children = {}
         self.wins = 0
-        self.visits = 0
+        self.N = 0
+        self.Q = 0
         
-        print(self.move)
+    def add_children(self, children):
+        
+        for child in children:
+            
+            self.children[child.move] = child
+            
+    def value(self, explore):
+        
         
