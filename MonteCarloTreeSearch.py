306bb4ae50cf793803992a6f1cd63873521f5b51
Kaneel <kaneel.senevirathne@gmail.com>
2021-11-25 14:01:00 -0500

    monte carlo updated

--- MonteCarloTreeSearch.py
+++ MonteCarloTreeSearch.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
+Monte carlo tree search for tic tac toe in Python.
+Author: Kaneel Senevirathne, November 2021
+
+"""
+
+
 import numpy as np
+from collections import defaultdict
+import math
 
 
 #monte carlo tree search for tic tac toe
@@ -9,67 +18,98 @@ class Node(object):
     def __init__(self, state):        
         self.state = state
         self.parent = None
-        self.children = []
+        self.children = defaultdict(int)
         self.W = 0
         self.N = 0
         self.Q = 0
         self.U = 0
 
 class MCTS(object):
-    def __init__(self, root):
-        self.root = Node(root)
+    def __init__(self, exploration_weight = 1):
+        self.Q = defaultdict(int)
+        self.N = defaultdict(int)
+        self.W = defaultdict(int)
+        self.U = defaultdict(int)
+        self.children = dict()
+        self.exploration_weight = exploration_weight
 
-    def select(self):
-        """
-        This function selects the best path starting from the root
-        """
-        return self.select_helper()
-
-    def select_helper(self):
-        """
-        helper function for select. this function selects the optimal path
-        """
-        #max state
-        max_val = -1000
+    def choose(self, node):
+        #check if its the terminal node
+        if node.is_terminal():
+            pass
         
-        if self.root.children:
-            for child in self.root.children:
-                val = child.Q + child.U
-
-                if val > max_val:
-                    max_node = child
-            return max_node
-        else:
-            return self.expand()
-
-    def expand(self):
-        """
-        if a leaf node is reached expand to find a new node
-        """
-        #avail moves
-        avail_moves = np.where(np.array(self.root.state) == 0)
-        new_move = np.random.choice(avail_moves[0])
+        #check if node is not in children
+        if node not in self.children:
+            #not in children. new node reached
+            return node
         
-        #create new state
-        new_state = self.root.state
-        new_state[new_move] = 1
-        print(self.root.state)
+        #if node is in self.children select the node with the maximum score
+        def score(n):
+            if self.N[n] == 0:
+                return float("-inf")
+            return self.Q[n] /self.N[n]
 
-        #create new node for the new expanded state
-        expanded_state = Node(new_state)
-        expanded_state.parent = self.root
+        return max(self.children[node], key = score)
 
-        #assign the new state to start states children
-        self.root.children.append(expanded_state)
-        
-        # print(start_state.state)
-        # print(expanded_state.state)
+    def rollout(self, node):
+        path = self._select_(node)
+        leaf = path[-1]
+        self._expand_(leaf)
+        reward = self._simulate_(leaf)
+        self._backpropagate_(path, reward)
 
-#start state
-start = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    def _select_(self, node):
+        #find an unexplored descendant
+        path = []
+        while True:
+            path.append(node)
+            if node not in self.children or not self.children[node]:
+                return path
+            
+            unexplored = self.children[node] - self.children.keys()
 
+            if unexplored:
+                n = unexplored.pop()
+                path.append(n)
+                return path
+            node = self._uctselect_(node)
 
-tree = MCTS(start)
+    def _expand_(self, node):
 
-print(tree.select())
+        if node in self.children:
+            pass
+        self.children[node] = node.find_children()
+
+    def _simulate_(self, node):
+        invert_reward = True
+        while True:
+            if node.is_terminal():
+                reward = node.reward()
+                return 1 - reward if invert_reward else reward
+
+            node = node.find_random_child()
+            invert_reward = not invert_reward
+
+    def _backpropagate_(self, path, reward):
+
+        for node in reversed(path):
+            self.N[node] += 1
+            self.Q[node] += reward
+            self.U[node] = None
+            self.W[node] = None
+            reward = 1 - reward
+
+    def _uctselect_(self, node):
+
+        assert all(n in self.children for n in self.children[node])
+
+        log_N = math.log(self.N[node])
+
+        def uct(n):
+
+            return self.Q[n]/self.N[n] + self.exploration_weight * math.sqrt(log_N / self.N[n])
+
+        return max(self.children[node], key = uct)
+
+class Node()
 
